2b992ca083030405beef0cd2f3c8a79fabe226b8
SagarSingh2003 <iamsagar762@gmail.com>
2023-12-03 13:55:17 +0530

    ConvertBoolToString Added

---
+++ 8kyu/ConvertBoolToString.js
@@ -0,0 +1,26 @@
+// https://www.codewars.com/kata/53369039d7ab3ac506000467/solutions/javascript?filter=me&sort=best_practice&invalids=false
+
+
+
+
+//Description:
+
+// Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+
+
+// Solution
+
+function boolToWord( bool ){
+    //...
+    
+    //Check if true or false
+    //If false return no
+    //else return true
+    
+    if (bool === true){
+        return "Yes"
+    }else{
+        return "No"
+    }
+    
+  }
